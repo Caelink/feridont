f53005460542fc194b15024aec1b9727e410b9fc
Caelin Jackson-King <caelink@gmail.com>
2017-04-02 19:10:35 -0400

    messing around

--- feriDoIt.js
+++ feriDoIt.js
@@ -1,7 +1,9 @@
 var s = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = chrome.extension.getURL('script.js');
+s.src = chrome.extension.getURL('start-laughter.js');
 s.onload = function() {
-    this.remove();
+	var myAudio = new Audio();        // create the audio object
+	myAudio.src = "ben-brode2_1.mp3"; // assign the audio file to its src
+	myAudio.play();                   // play the music
 };
 (document.head || document.documentElement).appendChild(s);
--- start-laughter.js
+++ start-laughter.js
@@ -1,1 +1,18 @@
-var feri = document.getElementById('feridun')
+function playLaugh(source) {
+	var laugh = new Audio();
+	if (source === 'brode') {
+		laugh.src = "laughter/ben.mp3";
+	} else if (source === 'feridun') {
+		laugh.src = "laughter/feridun.mp3";
+	}
+	laugh.play();
+}
+
+playLaugh('feridun')
+document.getElementById("brodeFace").addEventListener("click", function() {
+	playLaugh('brode');
+})
+
+document.getElementById("feridunFace").addEventListener("click", function() {
+	playLaugh('feridun');
+})
